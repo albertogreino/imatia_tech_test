207d24067da0c85e591ddb2ddf845214ac13016c
Alberto G. Reino <albertogreino@gmail.com>
2023-05-29 12:24:07 +0200

    Initial commit

---
+++ src/main/java/com/imatia/imatia_tech_test/ImatiaTechTestApplication.java
@@ -0,0 +1,13 @@
+package com.imatia.imatia_tech_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImatiaTechTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImatiaTechTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/imatia/imatia_tech_test/ImatiaTechTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imatia.imatia_tech_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImatiaTechTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
