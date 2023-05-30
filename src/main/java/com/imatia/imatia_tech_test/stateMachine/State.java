856ceeab8fbbdd10b690ccbba285114d15ec71d7
Alberto G. Reino <albertogreino@gmail.com>
2023-05-30 15:55:46 +0200

    Add State Machine

---
+++ src/main/java/com/imatia/imatia_tech_test/stateMachine/State.java
@@ -0,0 +1,8 @@
+package com.imatia.imatia_tech_test.stateMachine;
+
+public enum State {
+    RECOGIDO_EN_ALMACEN,
+    EN_REPARTO,
+    INCIDENCIA_EN_ENTREGA,
+    ENTREGADO;
+}
---
+++ src/main/java/com/imatia/imatia_tech_test/stateMachine/StateTransitionValidator.java
@@ -0,0 +1,22 @@
+package com.imatia.imatia_tech_test.stateMachine;
+
+public class StateTransitionValidator {
+    public static boolean isValidTransition(State from, State to) {
+        if (from == State.RECOGIDO_EN_ALMACEN
+                && to != State.RECOGIDO_EN_ALMACEN) {
+            return true;
+        } else if (to == State.RECOGIDO_EN_ALMACEN) {
+            return false;
+        } else if (from == State.ENTREGADO) {
+            return false;
+        } else if (from == State.EN_REPARTO &&
+                (to == State.INCIDENCIA_EN_ENTREGA || to == State.ENTREGADO)) {
+            return true;
+        } else if (from == State.INCIDENCIA_EN_ENTREGA &&
+                (to == State.EN_REPARTO || to == State.ENTREGADO)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
